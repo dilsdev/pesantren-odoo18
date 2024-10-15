e1cf02964c63e9d095da0541ca2055da772361bc
dilsdev <abdil150507@gmail.com>
2024-10-15 10:27:27 +0700

    Membuat module pesantren

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Pesantren',
+    'version': '1.0',
+    'summary': 'Pesantren',
+    'description': """
+        oke.
+    """,
+    'category': 'Pesantren',
+    'author': 'Dil',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu_views.xml',
+    ],
+    'sequence': -2,
+    'installable': True,
+    'application': True,
+}
